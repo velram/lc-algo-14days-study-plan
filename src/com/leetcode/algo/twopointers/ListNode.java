e7016e09e0723047b974a5fd3ab6fb0e843d9805
Coder <coder@foo.com>
2022-08-23 21:50:45 +0530

    Vel - Mid node in linked list

---
+++ src/com/leetcode/algo/twopointers/ListNode.java
@@ -0,0 +1,14 @@
+package com.leetcode.algo.twopointers;
+
+/**
+ * @author Vel
+ * @date 23/08/22 : 21:49
+ *
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
